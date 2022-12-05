4226cbd9446a9d3238e263f3b714226b08cf30f6
Andrey Gribanov <a.gribanov@staffcop.ru>
2022-12-05 14:29:39 +0700

    3.2 if-then-else construction

--- fixture/group.py
+++ fixture/group.py
@@ -10,10 +10,20 @@ class GroupHelper:
         drv = self.app.drv
         self.open_groups_page()
         drv.find_elements(By.CSS_SELECTOR, '[name=new]')[0].click()
-        drv.find_element(By.CSS_SELECTOR, '[name=group_name]').send_keys(group.name)
-        drv.find_element(By.CSS_SELECTOR, '[name=group_header]').send_keys(group.header)
-        drv.find_element(By.CSS_SELECTOR, '[name=group_footer]').send_keys(group.footer)
+        self.fill_group_form(drv, group)
         drv.find_element(By.CSS_SELECTOR, '[name=submit]').click()
+
+    def change_field_value(self, field_name, text):
+        drv = self.app.drv
+        if text is not None:
+            drv.find_element(By.CSS_SELECTOR, field_name).clear()
+            drv.find_element(By.CSS_SELECTOR, field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        drv = self.app.drv
+        self.change_field_value('[name=group_name]', group.name)
+        self.change_field_value('[name=group_header]', group.header)
+        self.change_field_value('[name=group_footer]', group.footer)
 
     def open_groups_page(self):
         drv = self.app.drv
@@ -22,8 +32,12 @@ class GroupHelper:
     def delete_first_group(self):
         drv = self.app.drv
         self.open_groups_page()
-        drv.find_element(By.NAME, 'selected[]').click()
+        self.select_first_group()
         drv.find_element(By.CSS_SELECTOR, '[name=delete]').click()
+
+    def select_first_group(self):
+        drv = self.app.drv
+        drv.find_element(By.NAME, 'selected[]').click()
 
     def delete_all_groups(self):
         drv = self.app.drv
@@ -32,3 +46,12 @@ class GroupHelper:
         for i in range(0, len(list_groups)):
             drv.find_elements(By.CSS_SELECTOR, '[method=post] .group input')[i].click()
         drv.find_elements(By.CSS_SELECTOR, '[name=delete]')[0].click()
+
+    def modify_first_group(self, new_group_data):
+        drv = self.app.drv
+        self.open_groups_page()
+        self.select_first_group()
+        drv.find_element(By.NAME, 'edit').click()
+        self.fill_group_form(new_group_data)
+        drv.find_element(By.NAME, 'update').click()
+        self.open_groups_page()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,13 +3,13 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="Name group modify"))
+    app.group.modify_first_group(Group(name="Name group modify"))
     app.group.open_groups_page()
     app.session.log_out()
 
 
 def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(header="Header group modify"))
+    app.group.modify_first_group(Group(header="Header group modify"))
     app.group.open_groups_page()
     app.session.log_out()
