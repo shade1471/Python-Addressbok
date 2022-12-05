52fb6f7abc4e753a035d0ebb9261ee1fb422ac09
Andrey Gribanov <a.gribanov@staffcop.ru>
2022-12-05 21:26:43 +0700

    4.2.2 assert lists group

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from model.group import Group
 
 
 class GroupHelper:
@@ -64,3 +65,17 @@ class GroupHelper:
         drv = self.app.drv
         self.open_groups_page()
         return len(drv.find_elements(By.NAME, 'selected[]'))
+
+    def get_group_list(self):
+        drv = self.app.drv
+        self.open_groups_page()
+        groups = []
+        for element in drv.find_elements(By.CSS_SELECTOR, 'span.group'):
+            text = element.text
+            id = element.find_element(By.NAME, 'selected[]').get_attribute('value')
+            groups.append(Group(name=text, id=id))
+
+        return groups
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
