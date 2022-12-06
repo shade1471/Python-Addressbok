2a2f0eb6f63c8403f21cdcca365a2bcd27ea7176
Andrey Gribanov <a.gribanov@staffcop.ru>
2022-12-06 15:52:39 +0700

    5.7

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,38 @@
+import pytest
+import pytest
+
 from model.group import Group
-from sys import maxsize
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [
+    Group(name='The best group!', header='My new header', footer='My new footer'),
+    Group(name=random_string("name", 10), header=random_string("header", 15), footer=random_string("footer", 20)),
+    Group(name='', header='', footer='')
+]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name='The best group!', header='My new header', footer='My new footer')
+    # group = Group(name='The best group!', header='My new header', footer='My new footer')
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name='', header='', footer='')
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(name='', header='', footer='')
+#     app.group.create(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
